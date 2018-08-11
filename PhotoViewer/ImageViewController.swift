38ecbc9190a6eb8d4151db51290e789838aa499f
Ilya Semerukhin <semerukhin@gmail.com>
2018-08-11 18:03:43 +0300

    Create main ViewController

---
+++ PhotoViewer/ImageViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ImageViewController.swift
+//  PhotoViewer
+//
+//  Created by Ilya Semerukhin on 11.08.2018.
+//
+
+import UIKit
+
+class ImageViewController: UIViewController {
+   
+   
+   
+}
